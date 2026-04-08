9e69aff02d29372e6f039cbafd318204c3cc2b16
Elay <ayseelaycengiz@gmail.com>
2026-04-08 14:09:20 +0100

    i drew where the doodler was going to be

--- doodlejump.js
+++ doodlejump.js
@@ -4,9 +4,37 @@ let boardWidth = 500;
 let boardHeight = 705;
 let context;
 
+//doodler
+let doodlerWidth = 46;
+let doodlerHeight = 46;
+let doodlerX = boardWidth/2 - doodlerWidth/2;
+let doodlerY = boardHeight*7/8 - doodlerHeight;
+let doodlerRightImg;
+let doodlerLeftImg;
+
+let doodler = {
+    img : null,
+    x : doodlerX,
+    y : doodlerY,
+    width : doodlerWidth,
+    height : doodlerHeight
+}
+
 window.onload = function() {
     board = document.getElementById("board");
     board.height = boardHeight;
     board.width = boardWidth;
     context = board.getContext("2d"); // used for drawing on the board
+
+    //draw doodler
+    context.fillStyle ="green";
+    context.fillRect(doodler.x, doodler.y, doodler.width, doodler.height);
+
+    //load images
+    doodlerRightImg = new Image();
+    doodlerRightImg.scr = "./doodler-right.png";
+    doodler.img = doodlerRightImg;
+    doodlerRightImg.onload = function(){
+        context.drawImage(doodler.img, doodler.x, doodler.y, doodler.height, doodler.width);
+    }
 }
